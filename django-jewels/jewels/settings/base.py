cf7b4d8161c31e69386da3960ceef542b04a2222
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-02-10 16:37:56 +0100

    adding a default set of templates

--- django-jewels/jewels/settings/base.py
+++ django-jewels/jewels/settings/base.py
@@ -77,7 +77,9 @@ ROOT_URLCONF = 'jewels.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
